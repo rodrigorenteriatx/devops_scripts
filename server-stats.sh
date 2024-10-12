3e91ac78c86b62273b8c15fa84a46b8e3f24f3b1
rodrigorenteriatx <rodrigorenteriatx@gmail.com>
2024-10-12 16:24:19 -0700

    changed %cmd field to comm , in ps commands

--- server-stats.sh
+++ server-stats.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+#Run with sudo, or root  (for lastb)
 #COLOR
 G='\033[32m'
 RST='\033[0m'
@@ -9,7 +10,7 @@ Y='\033[33m'
 
 echo -e "${Y}SERVER PERFORMANCE${RST}\n$(date +"%m/%d/%Y" && date +"%H:%M PDT")\n"
 echo -e "${G}---FAILED LOGINS---${RST}"
-sudo lastb | awk 'NR < 4'
+lastb | awk 'NR < 4'
 
 echo -e "\n${G}---LOGGED IN---${RST}"
 w -s
@@ -21,14 +22,16 @@ echo -e "${G}----CPU----${RST}"
 mpstat | awk 'NR > 3 {print "USED:"(100.0 - $13)"%"; print "FREE: "$13"%" }'
 
 echo -e "${G}----MEM----${RST}"
-free | grep Mem | awk '{mem=($3/$2); printf "USED: %.2f%\n", mem}'
-free | grep Mem | awk '{free=($4/$2); printf "FREE: %.2f% (Not in Buffer/Cache/Programs)\n",free}'
+free | grep Mem | awk '{mem=($3/$2 * 100); printf "USED: %.2f%\n", mem}'
+free | grep Mem | awk '{free=($4/$2 * 100); printf "FREE: %.2f% (Not in Buffer/Cache/Programs)\n",free}'
+
 #free -h | grep Mem | awk '{print "In GB: " $3; print "Free Memory: " $4}'
+
 
 echo -e "\n${G}----TOP 5 PROCESSES----${RST}"
 echo -e "${G}----CPU----${RST}"
-ps -eo pid,user,%cpu,cmd --sort=-%cpu | head -5
+ps -eo pid,user,%cpu,comm --sort=-%cpu | head -5
 
 echo -e "${G}----MEM----${RST}"
-ps -eo pid,user,%mem,cmd --sort=-%mem | head -5
+ps -eo pid,user,%mem,comm --sort=-%mem | head -5
 
